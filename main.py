2887094a5c0e73a6e48775c7ae4ccfc0d3783f6f
jayozer <jayozer@gmail.com>
2024-06-25 15:14:35 -0700

    medium analyzer with pinecone and pdf analyzer with faiss in memory

--- main.py
+++ main.py
@@ -8,7 +8,13 @@ from langchain import hub
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains.retrieval import create_retrieval_chain
 
+from langchain_core.runnables import RunnablePassthrough
+
+
 load_dotenv()
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
 
 
 if __name__ == "__main__":
@@ -26,6 +32,9 @@ if __name__ == "__main__":
         index_name=os.environ["INDEX_NAME"], embedding=embeddings
     )
 
+    # Two ways to retrieve information. Second template has "thanks for asking" to differentiate.
+    # Using the retrieval QA chat prompt
+
     retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
     combine_docs_chain = create_stuff_documents_chain(llm, retrieval_qa_chat_prompt)
     retrieval_chain = create_retrieval_chain(
@@ -34,3 +43,25 @@ if __name__ == "__main__":
 
     result = retrieval_chain.invoke(input={"input": query})
     print(result)
+
+   #use own architecture
+
+    template = """ Use the following pieces of context to answer the question at the end.
+    If you dont know the answer, just say that you dont know, dont try to make up an answer.
+    Use three sentences maximum and keep the answer as concise as possible.
+    Always say "thanks for asking!" at the end of the answer.
+    
+    {context}
+    
+    Question: {question}
+    
+    Helpful Answer:"""
+
+    custom_rag_prompt = PromptTemplate.from_template(template)
+
+    rag_chain = ({"context": vectorstore.as_retriever() | format_docs, "question" : RunnablePassthrough()}
+                 | custom_rag_prompt
+                 | llm)
+    
+    res = rag_chain.invoke(query)
+    print(res)
